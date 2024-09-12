e0654027be272e917229bc468eae89309def04ec
Gaston-Care <gecare@udc.edu.ar>
2024-09-12 17:51:29 -0300

    Se creó la vista 'CrearPublicacion' con la plantilla correspondiente.

--- post/urls.py
+++ post/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from post.views import home_view
+from post.views import home_view, CrearPublicacion
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('crear/publicacion', CrearPublicacion.as_view(), name='crear_publicacion')
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from post.models import Publicacion
 
 # Create your views here.
 def home_view(request):
     return render(request, 'home.html')
+
+class CrearPublicacion(CreateView):
+    model = Publicacion
+    fields = ['titulo','contenido',]
+    template_name = 'crear_publicacion.html'
+    success_url = reverse_lazy('publicaciones')
+
+    def form_valid(self, form):
+        form.instance.autor = self.request.user
+        return super().form_valid(form)
