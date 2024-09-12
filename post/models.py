437159cd53a1d01bb66f0eec93c22b0938c14e10
Gaston-Care <gecare@udc.edu.ar>
2024-09-12 17:10:31 -0300

    Se creó el modelo 'Publicacion' y se realizaron las migraciones

--- post/models.py
+++ post/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Publicacion(models.Model):
+    titulo = models.CharField(max_length=30)
+    contenido = models.TextField()
+    fecha_de_publicacion = models.DateTimeField(auto_now_add=True)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='publicaciones')
+
+    def __str__(self):
+        return self.titulo
