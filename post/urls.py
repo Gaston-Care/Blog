ad03ae3969f4635ef6170a509eaab4476d55a32c
Gaston-Care <gecare@udc.edu.ar>
2024-09-12 20:12:49 -0300

    Se añadió el login redirect en el archivo 'settings.py' e implementó código html.

--- post/urls.py
+++ post/urls.py
@@ -3,6 +3,6 @@ from post.views import home_view, CrearPublicacion,Publicaciones
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('crear/publicacion', CrearPublicacion.as_view(), name='crear_publicacion'),
+    path('publicaciones/crear', CrearPublicacion.as_view(), name='publicaciones_crear'),
     path('publicaciones/',Publicaciones.as_view(), name='publicaciones'),
 ]
