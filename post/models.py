19fee867392359ff2fe29da72c51464fbf8eb682
Gaston-Care <gecare@udc.edu.ar>
2024-09-13 18:35:17 -0300

    Se agregó styles en la plantilla 'publicaciones.html' y actualización longitud  en el campo 'titulo' del modelo.

--- post/models.py
+++ post/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Publicacion(models.Model):
-    titulo = models.CharField(max_length=30)
+    titulo = models.CharField(max_length=75)
     contenido = models.TextField()
     fecha_de_publicacion = models.DateTimeField(auto_now_add=True)
     autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='publicaciones')
